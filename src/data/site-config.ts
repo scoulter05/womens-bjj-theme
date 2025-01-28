9900bb8da0756c7987611fb1d37b2f064c42fa4f
scoulter05 <s.coulter555@gmail.com>
2025-01-27 21:03:33 -0500

    Update site-config.ts
    Updated that previous change for format

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -38,7 +38,7 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    title: 'MatQueensClub',
+    title: 'Soda City Mat Queens Club',
     subtitle: 'Minimal Astro.js theme',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
